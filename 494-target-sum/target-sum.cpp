d4ffa4c2df704eacda2b21a3156ab0cf3b0ef880
SANDEEP <sandeep24504@iiitd.ac.in>
2026-01-15 20:03:11 +0530

    Time: 183 ms (31.01%) | Memory: 59 MB (10.50%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
     int t;
-    int move(vector<int>&nums,int sum,int idx){
+    
+    unordered_map<int, unordered_map<int, int>> dp;
 
+    int move(vector<int>& nums, int sum, int idx) {
         
         
-
-        if(idx<0 ){
-            return sum==t;
+        if (idx < 0) {
+            return sum == t;
         }
 
-        int positive=move(nums,sum+nums[idx],idx-1);
-        int negative=move(nums,sum-nums[idx],idx-1);
+        
+        if (dp.count(idx) && dp[idx].count(sum)) {
+            return dp[idx][sum];
+        }
 
-        return positive+negative;
+        
+        int positive = move(nums, sum + nums[idx], idx - 1);
+        int negative = move(nums, sum - nums[idx], idx - 1);
+
+        
+        return dp[idx][sum] = positive + negative;
     }
-    int findTargetSumWays(vector<int>& nums, int target) {
 
-            t=target;
-            int n=nums.size();
-            return move(nums,0,n-1);
+    int findTargetSumWays(vector<int>& nums, int target) {
+        t = target;
+        dp.clear(); 
+        int n = nums.size();
+        return move(nums, 0, n - 1);
     }
 };
