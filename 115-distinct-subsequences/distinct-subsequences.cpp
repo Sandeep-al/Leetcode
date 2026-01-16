098dc133617cd5aa37ddc43fbc27191214aa0484
SANDEEP <sandeep24504@iiitd.ac.in>
2026-01-16 13:03:23 +0530

    Time: 39 ms (18.18%) | Memory: 27 MB (62.76%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
-    int move(string& s, string& t, int idx1, int idx2, vector<vector<int>>& dp) {
+    int move(string&s, string&t,int idx1,int idx2,vector<vector<int>>&dp){
 
-      
-        if (idx2 < 0) return 1;
-        if (idx1 < 0) return 0; 
+        if(idx2<0) return 1;
+        if(idx1<0) return 0;
 
-       
-        if (dp[idx1][idx2] != -1) {
+        if(dp[idx1][idx2]!=-1){
             return dp[idx1][idx2];
         }
+        int pick=0;
+        int notpick=0;
+        if(s[idx1]==t[idx2])//matching i have two options
+        {
+            //pick
+            pick=move(s,t,idx1-1,idx2-1,dp);
+            notpick=move(s,t,idx1-1,idx2,dp);
 
-        
-        int result = 0;
-        if (s[idx1] == t[idx2]) {
-            
-            int pick = move(s, t, idx1 - 1, idx2 - 1, dp);
-            int notPick = move(s, t, idx1 - 1, idx2, dp);
-            result = pick + notPick;
-        } else {
-            
-            result = move(s, t, idx1 - 1, idx2, dp);
+            return dp[idx1][idx2]=pick+notpick;
         }
 
-        
-        return dp[idx1][idx2] = result;
-    }
 
+        return dp[idx1][idx2]=move(s,t,idx1-1,idx2,dp);
+    }
     int numDistinct(string s, string t) {
-        int m = s.size();
-        int n = t.size();
+
+
+        int m=s.size();
+        int n=t.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return move(s,t,m-1,n-1,dp);
         
-        
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        
-        return move(s, t, m - 1, n - 1, dp);
     }
 };
