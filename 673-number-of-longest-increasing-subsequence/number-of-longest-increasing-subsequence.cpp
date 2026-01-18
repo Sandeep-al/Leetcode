f4a8b2cfb34fc575abb4bfaa635a5f408e000d93
SANDEEP <sandeep24504@iiitd.ac.in>
2026-01-18 15:37:47 +0530

    Time: 22 ms (87.58%) | Memory: 17.4 MB (68.30%) - LeetSync

---
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        vector<int> cnt(n, 1);
+
+        int maxi = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+
+                if (nums[i] > nums[j]) {
+
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        cnt[i] = cnt[j];
+                    }
+
+                    else if (dp[j] + 1 == dp[i]) {
+                        cnt[i] += cnt[j];
+                    }
+                }
+            }
+            maxi = max(maxi, dp[i]);
+        }
+
+        int totalNumberOfLIS = 0;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] == maxi) {
+                totalNumberOfLIS += cnt[i];
+            }
+        }
+
+        return totalNumberOfLIS;
+    }
+};
