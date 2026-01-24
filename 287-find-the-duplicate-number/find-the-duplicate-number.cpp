2fe8bfaf767591eecb7d0bca0640f9a590243209
SANDEEP <sandeep24504@iiitd.ac.in>
2026-01-24 22:04:25 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (40.77%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow=nums[0];
+        int fast=nums[0];
+
+        while(true){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            if(fast==slow){
+                break;
+            }
+        }
+
+        int ptr1=nums[0];
+        int ptr2=slow;
+
+        while(ptr1!=ptr2){
+            ptr1=nums[ptr1];
+            ptr2=nums[ptr2];
+        }
+
+        return ptr1;
+    }
+};
