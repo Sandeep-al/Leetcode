d7677d8aa05e4772ec6fd7df80cd199ea90e24ea
SANDEEP <sandeep24504@iiitd.ac.in>
2026-03-04 09:02:13 +0530

    Time: 1982 ms (5.02%) | Memory: 475 MB (5.02%) - LeetSync

--- 4219-minimum-operations-to-make-array-parity-alternating/minimum-operations-to-make-array-parity-alternating.cpp
+++ 4219-minimum-operations-to-make-array-parity-alternating/minimum-operations-to-make-array-parity-alternating.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
+
         int k = nums.size();
 
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        // {value, list_index, element_index}
+        priority_queue<array<int, 3>, vector<array<int, 3>>,
+                       greater<array<int, 3>>>
+            pq;
 
         int current_max = INT_MIN;
 
@@ -14,7 +18,8 @@ public:
 
         vector<int> range = {INT_MIN, INT_MAX};
 
-        while (true) {
+        while (pq.size() == k) {
+
             auto curr = pq.top();
             pq.pop();
 
@@ -22,11 +27,12 @@ public:
             int list_idx = curr[1];
             int elem_idx = curr[2];
 
-            if ((long long)current_max - min_value < (long long)range[1] - range[0]) {
+            if (current_max - min_value <
+                (long long)range[1] - (long long)range[0]) {
                 range = {min_value, current_max};
             }
 
-            if (elem_idx + 1 < (int)nums[list_idx].size()) {
+            if (elem_idx + 1 < nums[list_idx].size()) {
                 int next_val = nums[list_idx][elem_idx + 1];
                 pq.push({next_val, list_idx, elem_idx + 1});
                 current_max = max(current_max, next_val);
@@ -37,59 +43,65 @@ public:
 
         return range;
     }
-
     vector<int> makeParityAlternating(vector<int>& nums) {
-        int n = nums.size();
-        int count1 = 0, count2 = 0;
 
-        vector<vector<int>> solve1, solve2;
-
-        // Pass 1: index 0 = even, index 1 = odd, ...
-        for (int i = 0; i < n; i++) {
-            int expected = i % 2; // 0=even, 1=odd
-            if (abs(nums[i]) % 2 != expected) {
+        int mini = INT_MAX;
+        int count1 = 0;
+        int count2 = 0;
+        int curr = 0;
+        vector<vector<int>> solve1;
+        vector<vector<int>> solve2;
+        for (auto it : nums) {
+            if (abs(it) % 2 != curr) {
                 count1++;
-                solve1.push_back({nums[i] - 1, nums[i] + 1});
-            } else {
-                solve1.push_back({nums[i]});
-            }
-        }
+                vector<int> options;
 
-        // Pass 2: index 0 = odd, index 1 = even, ...
-        for (int i = 0; i < n; i++) {
-            int expected = 1 - (i % 2); // 1=odd, 0=even
-            if (abs(nums[i]) % 2 != expected) {
+                if (it > INT_MIN)
+                    options.push_back(it - 1);
+
+                if (it < INT_MAX)
+                    options.push_back(it + 1);
+
+                solve1.push_back(options);
+            } else {
+                solve1.push_back({it});
+            }
+            curr = 1 - curr;
+        }
+        curr = 1;
+        for (auto& jt : nums) {
+            if (abs(jt) % 2 != curr) {
                 count2++;
-                solve2.push_back({nums[i] - 1, nums[i] + 1});
-            } else {
-                solve2.push_back({nums[i]});
-            }
-        }
+                vector<int> options;
 
-        // Early exit: already alternating
-        // spread = max - min of original array
-        if (count1 == 0) {
-            int mn = *min_element(nums.begin(), nums.end());
-            int mx = *max_element(nums.begin(), nums.end());
-            return {0, mx - mn};
-        }
-        if (count2 == 0) {
-            int mn = *min_element(nums.begin(), nums.end());
-            int mx = *max_element(nums.begin(), nums.end());
-            return {0, mx - mn};
+                if (jt > INT_MIN)
+                    options.push_back(jt - 1);
+
+                if (jt < INT_MAX)
+                    options.push_back(jt + 1);
+
+                solve2.push_back(options);
+            } else {
+                solve2.push_back({jt});
+            }
+            curr = 1 - curr;
         }
 
         if (count1 < count2) {
+
             auto it = smallestRange(solve1);
             return {count1, it[1] - it[0]};
-        } else if (count2 < count1) {
+        } else if (count1 > count2) {
+
             auto it = smallestRange(solve2);
             return {count2, it[1] - it[0]};
-        } else {
-            // count1 == count2
-            auto it = smallestRange(solve1);
-            auto jt = smallestRange(solve2);
-            return {count1, min(it[1] - it[0], jt[1] - jt[0])};
         }
+
+        auto it = smallestRange(solve1);
+        auto jt = smallestRange(solve2);
+
+        int mini2 = min(it[1] - it[0], jt[1] - jt[0]);
+
+        return {count1, mini2};
     }
 };
