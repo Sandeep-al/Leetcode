39529a9ac9fdbc3e1fdd36be9ca14344fcf903c0
SANDEEP <sandeep24504@iiitd.ac.in>
2026-03-22 13:14:23 +0530

    Time: 4 ms (56.31%) | Memory: 17.8 MB (95.08%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,51 +1,37 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& adj, vector<int>& vis, int node,
-             stack<int>& st) {
-
-        vis[node] = 1;
-
-        for (auto it : adj[node]) {
-
-            if (vis[it] == 1)
-                return true;
-
-            if (vis[it] == 0) {
-                if (dfs(adj, vis, it, st))
-                    return true;
-            }
-        }
-
-        vis[node] = 2;
-        st.push(node);
-        return false;
-    }
-
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-
+        
         int V = numCourses;
-        stack<int> st;
-        vector<vector<int>> adj(V);
-        vector<int> vis(V, 0);
+        vector<int> adj[V];
+        vector<int> indegree(V, 0);
+        vector<int> final_ans;
+        queue<int>q;
 
         for (auto& it : prerequisites) {
             adj[it[1]].push_back(it[0]);
+            indegree[it[0]]++;
         }
 
         for (int i = 0; i < V; i++) {
-            if (vis[i] == 0) {
-                if (dfs(adj, vis, i, st)) {
-                    return {};
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int node=q.front();q.pop();
+            final_ans.push_back(node);
+
+            for(auto &jt:adj[node]){
+                indegree[jt]--;
+                if(indegree[jt]==0){
+                    q.push(jt);
                 }
             }
         }
 
-        vector<int> final_ans;
-        while (!st.empty()) {
-            final_ans.push_back(st.top());
-            st.pop();
-        }
-
+        if(final_ans.size()<V) return {};
         return final_ans;
     }
 };
