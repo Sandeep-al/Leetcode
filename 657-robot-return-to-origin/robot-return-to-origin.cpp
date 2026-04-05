683ff13026706bbbb8d7538252077369ba911b10
SANDEEP <sandeep24504@iiitd.ac.in>
2026-04-05 16:19:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (13.45%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int left=0;
+        int right=0;
+        int up=0;
+        int down=0;
+
+        for(int i=0;i<moves.size();i++){
+            if(moves[i]=='U') up++;
+            else if(moves[i]=='L') left++;
+            else if(moves[i]=='R') right++;
+            else down++;
+        }
+
+        return left==right && up==down;
+    }
+};
