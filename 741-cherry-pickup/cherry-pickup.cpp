165992c3cfa928f1a1e2d510f0741bee28cc7776
SANDEEP <sandeep24504@iiitd.ac.in>
2026-04-05 22:11:29 +0530

    Time: 52 ms (41.98%) | Memory: 28.6 MB (37.61%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.cpp
+++ 741-cherry-pickup/cherry-pickup.cpp
@@ -3,53 +3,52 @@ public:
     vector<vector<int>> grid;
     vector<vector<vector<int>>> dp;
     int n;
-
-    int solve(int i1, int j1, int k) {
-
-        int j2 = i1 + j1 - k;
-
-        if (i1 >= n || j1 >= n || k >= n || j2 >= n || grid[i1][j1] == -1 ||
-            grid[k][j2] == -1) {
+    
+    int solve(int row1, int col1, int row2) {
+        int col2 = row1 + col1 - row2;
+        
+        if (row1 >= n || col1 >= n || row2 >= n || col2 >= n || 
+            grid[row1][col1] == -1 || grid[row2][col2] == -1) {
             return INT_MIN;
         }
-
-        if (i1 == n - 1 && j1 == n - 1) {
-            return grid[i1][j1];
+        
+        if (row1 == n - 1 && col1 == n - 1) {
+            return grid[row1][col1];
         }
-
-        if (dp[i1][j1][k] != -1) {
-            return dp[i1][j1][k];
+        
+        if (dp[row1][col1][row2] != -1) {
+            return dp[row1][col1][row2];
         }
-
+        
         int cherry = 0;
-        cherry += grid[i1][j1];
-
-        if (i1 != k || j1 != j2) {
-            cherry += grid[k][j2];
+        cherry += grid[row1][col1];
+        
+        if (row1 != row2 || col1 != col2) {
+            cherry += grid[row2][col2];
         }
-
-        int down_down = solve(i1 + 1, j1, k + 1);
-        int down_right = solve(i1 + 1, j1, k);
-        int right_down = solve(i1, j1 + 1, k + 1);
-        int right_right = solve(i1, j1 + 1, k);
-
+        
+        int down_down = solve(row1 + 1, col1, row2 + 1);
+        int down_right = solve(row1 + 1, col1, row2);
+        int right_down = solve(row1, col1 + 1, row2 + 1);
+        int right_right = solve(row1, col1 + 1, row2);
+        
         int next = max({down_down, down_right, right_down, right_right});
-
+        
         if (next == INT_MIN) {
-            return dp[i1][j1][k] = INT_MIN;
+            return dp[row1][col1][row2] = INT_MIN;
         }
-
-        return dp[i1][j1][k] = cherry + next;
+        
+        return dp[row1][col1][row2] = cherry + next;
     }
-
+    
     int cherryPickup(vector<vector<int>>& grid) {
         this->grid = grid;
         n = grid.size();
-
+        
         dp.assign(n, vector<vector<int>>(n, vector<int>(n, -1)));
-
+        
         int ans = solve(0, 0, 0);
-
+        
         return max(ans, 0);
     }
 };
