fbf6e2c1a846e2766224594cc57314166ce9f89d
SANDEEP <sandeep24504@iiitd.ac.in>
2026-05-01 16:13:58 +0530

    Time: 1898 ms (5.85%) | Memory: 104.9 MB (5.23%) - LeetSync

---
+++ 396-rotate-function/rotate-function.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        // lets start
+        // calculate F0
+        int n = nums.size();
+        int f0 = 0;
+        int total_sum = 0;
+        for (int i = 0; i < n; i++) {
+            f0 += (i * nums[i]);
+            total_sum += nums[i];
+        }
+        int maxi = f0;
+        vector<int> dp(n, 0);
+        dp[0] = f0;
+        for (int x = 1; x < n; x++) {
+            dp[x] = dp[x - 1] + ((1 - n) * (nums[n-x])) + total_sum - nums[n-x];
+            maxi = max(maxi, dp[x]);
+        }
+
+        for(int i=0;i<n;i++){
+            cout<<dp[i]<<endl;
+        }
+
+        return maxi;
+    }
+};
