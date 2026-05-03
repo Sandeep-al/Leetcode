e40c118b4f335a4ccab89b4fc7b445a3b91fa21e
SANDEEP <sandeep24504@iiitd.ac.in>
2026-05-03 13:51:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (36.38%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        
+       
+        if(goal.length()!=s.length() ){
+            return false;
+        }
+         s=s+s;
+        if(s.find(goal) != string::npos){
+            return true;
+        }
+
+        return false;
+
+       
+    }
+};
