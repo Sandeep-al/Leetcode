ed91071f7f9e6ea467abc3eec79caec0f71432d2
SANDEEP <sandeep24504@iiitd.ac.in>
2026-05-10 00:37:34 +0530

    Time: 85 ms (18.67%) | Memory: 82.6 MB (32.82%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
+    int n, k;
     vector<vector<int>> final_ans;
-    void solve(int k, int n, vector<int>& ans, int prev) {
-        if (k == 0) {
-            final_ans.push_back(ans);
+    vector<int>vis;
+    void solve(vector<int>& temp) {
+        if (temp.size() == k) {
+            final_ans.push_back(temp);
             return;
         }
+        int x = temp.size();
+        int last_added;
+        if (x == 0)
+            last_added = 0;
+        else
+            last_added = temp[x - 1];
 
-        for (int i = prev+1; i <= n; i++) {
+        for (int i = last_added + 1; i <= n; i++) {
+            if (!vis[i]) {
+                vis[i] = 1;
+                temp.push_back(i);
+                solve(temp);
+                temp.pop_back();
 
-            
-
-            ans.push_back(i);
-            solve(k - 1, n, ans, i);
-            ans.pop_back();
+                vis[i] = 0;
+            }
         }
     }
     vector<vector<int>> combine(int n, int k) {
-        vector<int> ans;
-        solve(k, n, ans, 0);
+        this->k = k;
+        this->n = n;
+        vis.assign(n+1,0);
+        vector<int> temp;
+        solve(temp); 
         return final_ans;
     }
 };
