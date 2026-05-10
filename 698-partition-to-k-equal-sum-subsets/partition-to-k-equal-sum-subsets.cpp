c3b0c0c22c2cd514423337fae65fc9dbec95d436
SANDEEP <sandeep24504@iiitd.ac.in>
2026-05-10 14:20:29 +0530

    Time: 440 ms (27.18%) | Memory: 11.9 MB (86.46%) - LeetSync

---
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int target;
+    int k;
+    int n;
+    vector<int> buckets;
+    vector<int> nums;
+    bool solve(int idx) {
+        if (idx == n)
+            return true;
+        // can i put this number in any of the bucket
+        for (int i = 0; i < k; i++) {
+            int curr = buckets[i];
+            if (curr + nums[idx] <= target) {
+                buckets[i] += nums[idx];
+
+                if (solve(idx + 1))
+                    return true;
+
+                buckets[i] -= nums[idx];
+            }
+
+            if (buckets[i] == 0)
+                break;
+        }
+
+        return false;
+    }
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        this->k = k;
+        this->nums = nums;
+        n = nums.size();
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+        }
+
+        if (sum % k == 0)
+            target = sum / k;
+        else
+            return false;
+
+        buckets.assign(k, 0);
+        return solve(0);
+    }
+};
