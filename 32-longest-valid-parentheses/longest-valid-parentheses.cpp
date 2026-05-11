db27134446b96cc079e48e3ab5ecad3e00c3d14a
SANDEEP <sandeep24504@iiitd.ac.in>
2026-05-11 10:58:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (98.34%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,60 +1,42 @@
 class Solution {
 public:
-    string s;
-    int n;
+    int longestValidParentheses(string s) {
+        int l = 0;
+        int r = 0;
+        int maxi = 0;
 
-    vector<int> dp;
+        for (int i = 0; i < s.size(); i++) {
 
-    int solve(int i) {
-
-        if (i >= n)
-            return 0;
-
-        if (dp[i] != -1)
-            return dp[i];
-
-        int ans = 0;
-
-        if (s[i] == '(') {
-
-            if (i + 1 < n && s[i + 1] == ')') {
-
-                ans = 2 + solve(i + 2);
+            if (s[i] == '(')
+                l++;
+            if (s[i] == ')')
+                r++;
+            if (r == l) {
+                maxi = max(maxi, l * 2);
             }
-
-            else if (i + 1 < n) {
-
-                int inside_len = solve(i + 1);
-
-                int matching_index = i + inside_len + 1;
-
-                if (matching_index < n && s[matching_index] == ')') {
-
-                    ans = inside_len + 2;
-
-                    ans += solve(matching_index + 1);
-                }
+            if (r > l) {
+                r = 0;
+                l = 0;
             }
         }
 
-        return dp[i] = ans;
-    }
+        l = 0;
+        r = 0;
 
-    int longestValidParentheses(string str) {
+        for (int i = s.size() - 1; i >= 0; i--) {
 
-        s = str;
-
-        n = s.size();
-
-        dp.assign(n, -1);
-
-        int final_ans = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            final_ans = max(final_ans, solve(i));
+            if (s[i] == '(')
+                l++;
+            if (s[i] == ')')
+                r++;
+            if (r == l) {
+                maxi = max(maxi, l * 2);
+            }
+            if (l > r) {
+                r = 0;
+                l = 0;
+            }
         }
-
-        return final_ans;
+        return maxi;
     }
 };
