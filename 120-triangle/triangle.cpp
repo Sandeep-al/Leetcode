bd0c55560f6e235b43c30e7489ebaade763001c8
SANDEEP <sandeep24504@iiitd.ac.in>
2026-05-15 12:10:51 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (7.79%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
+    vector<vector<int>> triangle;
+    vector<vector<int>> dp;
+    vector<vector<bool>> vis;
+
     int n;
-    int move(vector<vector<int>>& triangle,int idx1,int idx2,vector<vector<int>>&dp){
-        
 
-        if(idx1==n-1){
-            return triangle[idx1][idx2];
-        }
-        if(dp[idx1][idx2]!=-1e9){
-            return dp[idx1][idx2];
-        }
-        //going down
+    int solve(int row, int col) {
 
-        int down=move(triangle,idx1+1,idx2,dp);
-        int downright=move(triangle,idx1+1,idx2+1,dp);
+        if (row == n - 1)
+            return triangle[row][col];
 
-        return dp[idx1][idx2]=triangle[idx1][idx2]+min(down,downright);
+        if (vis[row][col])
+            return dp[row][col];
 
+        vis[row][col] = true;
 
+        return dp[row][col] =
+                   triangle[row][col] +
+                   min(solve(row + 1, col),
+                       solve(row + 1, col + 1));
     }
+
     int minimumTotal(vector<vector<int>>& triangle) {
-        
-        n=triangle.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1e9));
 
-        return move(triangle,0,0,dp);
+        n = triangle.size();
 
+        this->triangle = triangle;
+
+        dp.assign(n, vector<int>(n, 0));
+        vis.assign(n, vector<bool>(n, false));
+
+        return solve(0, 0);
     }
 };
