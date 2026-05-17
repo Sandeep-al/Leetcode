2e8ce7807dee2be3289bf74fbdb94ec4e6b500d1
SANDEEP <sandeep24504@iiitd.ac.in>
2026-05-17 14:22:44 +0530

    Time: 0 ms (100.00%) | Memory: 63.5 MB (78.39%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -3,8 +3,8 @@ public:
     int peakIndexInMountainArray(vector<int>& nums) {
 
 
-        int n = nums.size() - 1;
-        int high = n;
+        int n = nums.size();
+        int high = n-1;
         int low = 0;
 
         while (low <= high) {
@@ -12,7 +12,7 @@ public:
             int mid = low + (high - low) / 2;
 
             int left = (mid == 0) ? -1 : nums[mid - 1];
-            int right = (mid == high) ? -1 : nums[mid + 1];
+            int right = (mid == n-1) ? -1 : nums[mid + 1];
 
             if (nums[mid] > left &&
                 nums[mid] < right) { // positive slope go right
