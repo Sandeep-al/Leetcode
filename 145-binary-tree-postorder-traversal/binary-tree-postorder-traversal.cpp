1f48daa893e1687cc4fe7b6c74aef580588c303e
SANDEEP <sandeep24504@iiitd.ac.in>
2026-05-22 13:20:51 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (72.69%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -6,27 +6,40 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    vector<int>final_ans;
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> st;
+        if (root == nullptr)
+            return ans;
+        TreeNode* curr = root;
+        TreeNode* lastVisited = nullptr;
 
-    void compute(TreeNode*root){
+        while (curr || !st.empty()) {
 
-        if(root==nullptr){
-            return;
+            while (curr != nullptr) {
+                st.push(curr);
+
+                curr = curr->left;
+            }
+
+            curr = st.top();
+
+            if (curr->right == nullptr || curr->right == lastVisited) {
+                ans.push_back(curr->val);
+                st.pop();
+                lastVisited = curr;
+                curr = nullptr;
+            } else {
+                curr = curr->right;
+            }
         }
 
-         
-        compute(root->left);
-        
-        compute(root->right);
-        final_ans.push_back(root->val);
-    }
-    vector<int> postorderTraversal(TreeNode* root) {
-        compute(root);
-        return final_ans;
+        return ans;
     }
 };
