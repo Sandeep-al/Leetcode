b2ca900e0dde9bd18e85bbf85908894abad53fe3
SANDEEP <sandeep24504@iiitd.ac.in>
2026-06-11 12:48:22 +0530

    Time: 407 ms (33.56%) | Memory: 386.3 MB (15.44%) - LeetSync

--- 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.cpp
+++ 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.cpp
@@ -13,12 +13,10 @@ public:
             return dp[n][parity];
 
         if (parity == 1) {
-            return dp[n][1] =
-                (1LL * solve(n - 1, 0) + solve(n - 1, 1)) % MOD;
+            return dp[n][1] = (1LL * solve(n - 1, 0) + solve(n - 1, 1)) % MOD;
         }
 
-        return dp[n][0] =
-            (1LL * solve(n - 1, 1) + solve(n - 1, 0)) % MOD;
+        return dp[n][0] = (1LL * solve(n - 1, 1) + solve(n - 1, 0)) % MOD;
     }
 
     int maxdepth(int node, int parent) {
@@ -33,6 +31,21 @@ public:
         return 1 + maxi;
     }
 
+    int modPow(long long a, long long b) {
+        long long res = 1;
+        long long MOD = 1e9 + 7;
+
+        while (b > 0) {
+            if (b & 1)
+                res = (res * a) % MOD;
+
+            a = (a * a) % MOD;
+            b >>= 1;
+        }
+
+        return (int)res;
+    }
+
     int assignEdgeWeights(vector<vector<int>>& edges) {
         int nodes = edges.size() + 1;
 
@@ -45,6 +58,9 @@ public:
         }
 
         int maxi = maxdepth(0, -1) - 1;
-        return solve(maxi, 1);
+        return modPow(2, maxi - 1);
+        // return solve(maxi, 1);
+
+        
     }
 };
