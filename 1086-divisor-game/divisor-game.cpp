37895b90e1c10588254f5f13b95d03ea191a1144
SANDEEP <sandeep24504@iiitd.ac.in>
2026-07-13 16:25:09 +0530

    Time: 10 ms (2.33%) | Memory: 7.7 MB (58.30%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -23,6 +23,27 @@ public:
     bool divisorGame(int n) {
 
         memset(dp, -1, sizeof(dp));
-        return can_alice_win(n, 1);
+
+        dp[0][0] = 0;
+        dp[0][1] = 0;
+
+        for (int idx = 1; idx <= n; idx++) {
+            for (int alice = 0; alice <= 1; alice++) {
+                dp[idx][alice] = 0 ;
+                for (int i = 1; i < idx; i++) {
+                    if (idx % i == 0) {
+
+                        if (dp[idx - i][1 - alice] == 0) {
+                            dp[idx][alice] = 1;
+                            
+                        }
+                    }
+                }
+
+                
+            }
+        }
+
+        return dp[n][1];
     }
 };
