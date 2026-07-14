67b30b1377d5cdb0c0408981a049e682bc154ce3
SANDEEP <sandeep24504@iiitd.ac.in>
2026-07-14 12:43:08 +0530

    Time: 2195 ms (5.21%) | Memory: 59.2 MB (60.00%) - LeetSync

---
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int>nums;
+    int n;
+    const int mod=1e9+7;
+    int dp[202][202][202];
+    int solve(int idx,int curr_gcd1,int curr_gcd2){
+        if(idx==n){
+            return (curr_gcd1==curr_gcd2 && curr_gcd1!=0);
+        }
+
+        if(dp[idx][curr_gcd1][curr_gcd2]!=-1){
+            return dp[idx][curr_gcd1][curr_gcd2];
+        }
+        int new_curr_gcd1=gcd(curr_gcd1,nums[idx]);
+        int new_curr_gcd2=gcd(curr_gcd2,nums[idx]);
+
+        int ans=0;
+
+        //pick it up for 1
+        ans=(ans+solve(idx+1,new_curr_gcd1,curr_gcd2))%mod;
+        ans=(ans+solve(idx+1,curr_gcd1,new_curr_gcd2))%mod;
+        ans=(ans+solve(idx+1,curr_gcd1,curr_gcd2))%mod;
+
+        return dp[idx][curr_gcd1][curr_gcd2]=ans;
+    }
+    int subsequencePairCount(vector<int>& nums) {
+        this->nums=nums;
+        n=nums.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,0);
+    }
+};
