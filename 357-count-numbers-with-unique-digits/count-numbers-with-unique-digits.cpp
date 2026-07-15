9e3431f88fa9ae1a73729fc61f2f97d8b346262a
SANDEEP <sandeep24504@iiitd.ac.in>
2026-07-15 22:37:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (73.23%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int n;
-
+    int dp[9][1050];
     int solve(int idx, int bitmask) {
 
         if (idx == n)
             return 0;
-
+        if(dp[idx][bitmask]!=-1) return dp[idx][bitmask];
         int ans = 0;
 
         for (int i = 0; i < 10; i++) {
@@ -20,14 +20,14 @@ public:
             }
         }
 
-        return ans;
+        return dp[idx][bitmask]=ans;
     }
 
     int countNumbersWithUniqueDigits(int n) {
         this->n = n;
         if (n == 0)
             return 1;
-
+        memset(dp,-1,sizeof(dp));
         return solve(0, 0) + 1;
     }
 };
