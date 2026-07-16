01d940de314b6cd71cd6578f2006b5ca2c32855b
SANDEEP <sandeep24504@iiitd.ac.in>
2026-07-16 11:46:45 +0530

    Time: 67 ms (85.87%) | Memory: 155.5 MB (87.10%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
@@ -10,9 +10,7 @@ public:
         }
 
         sort(prefixgcd.begin(),prefixgcd.end());
-        for(int i=0;i<n;i++){
-            cout<<prefixgcd[i]<<" ";
-        }
+        
         long long sum=0;
         int i=0;
         int j=n-1;
