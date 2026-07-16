7b4110f7a5f9645a9cc960f0f92c3de1f5c31fc1
SANDEEP <sandeep24504@iiitd.ac.in>
2026-07-16 11:44:31 +0530

    Time: 2472 ms (5.53%) | Memory: 155.6 MB (87.10%) - LeetSync

---
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long gcdSum(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>prefixgcd(n);
+        int max_till_now=INT_MIN;
+        for(int i=0;i<n;i++){
+            max_till_now=max(max_till_now,nums[i]);
+            prefixgcd[i]=gcd(max_till_now,nums[i]);
+        }
+
+        sort(prefixgcd.begin(),prefixgcd.end());
+        for(int i=0;i<n;i++){
+            cout<<prefixgcd[i]<<" ";
+        }
+        long long sum=0;
+        int i=0;
+        int j=n-1;
+
+        while(i<j){
+            sum+=gcd(prefixgcd[i],prefixgcd[j]);
+            i++;
+            j--;
+        }
+
+        return sum;
+    }
+};
