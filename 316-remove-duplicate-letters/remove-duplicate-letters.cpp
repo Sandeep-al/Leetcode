2c216a2ec1e7e197c87d79b5289a9e0d016af576
SANDEEP <sandeep24504@iiitd.ac.in>
2026-07-19 17:59:45 +0530

    Time: 3 ms (16.72%) | Memory: 9.8 MB (17.91%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int last_idx[26];
+
+        for (int i = 0; i < s.size(); i++) {
+            last_idx[s[i] - 'a'] = i;
+        }
+
+        stack<char> st;
+        unordered_set<char> seen;
+
+        for (int i = 0; i < s.size(); i++) {
+
+            if (seen.find(s[i]) != seen.end()) {
+                continue;
+            }
+
+            while (!st.empty() && s[i] < st.top() &&
+                   last_idx[st.top() - 'a'] > i) {
+
+                seen.erase(st.top());
+                st.pop();
+            }
+
+            st.push(s[i]);
+            seen.insert(s[i]);
+        }
+
+        string s1 = "";
+        while (!st.empty()) {
+            s1.push_back(st.top());
+            st.pop();
+        }
+
+        reverse(s1.begin(), s1.end());
+        return s1;
+    }
+};
