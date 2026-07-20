85bac2ef7b86aac04514d70c34b1529f253da9a5
SANDEEP <sandeep24504@iiitd.ac.in>
2026-07-20 10:56:58 +0530

    Time: 80 ms (36.36%) | Memory: 83.4 MB (27.27%) - LeetSync

--- 4088-minimum-cost-to-convert-string-iii/minimum-cost-to-convert-string-iii.cpp
+++ 4088-minimum-cost-to-convert-string-iii/minimum-cost-to-convert-string-iii.cpp
@@ -6,20 +6,25 @@ public:
     vector<int> costs;
     int n;
     int dp[5001];
-    int check(int start, int len, string replacement, string pattern) {
+
+    int check(int start, int len, const string &replacement,
+              const string &pattern) {
 
         string source_sub = source.substr(start, len);
         string target_sub = target.substr(start, len);
 
-        // x->y   //y==replacment
+        // x->y   // y == replacement
         bool match = true;
 
         if (source_sub.size() != pattern.size() ||
             target_sub.size() != replacement.size()) {
             return -1;
         }
+
         int cost = 0;
+
         for (int i = 0; i < len; i++) {
+
             if (pattern[i] == source_sub[i]) {
                 continue;
             } else if (pattern[i] == '*') {
@@ -35,22 +40,27 @@ public:
 
         if (!match)
             return -1;
+
         return cost;
     }
+
     int solve(int idx) {
 
         if (idx >= n)
             return 0;
-        if (dp[idx] != -5) {
+
+        if (dp[idx] != -5)
             return dp[idx];
-        }
+
         int curr_cost = INT_MAX;
 
         for (int i = 0; i < rules.size(); i++) {
 
-            auto rule = rules[i];
-            string pattern = rule[0];
-            string replacement = rule[1];
+            const auto &rule = rules[i];
+
+            const string &pattern = rule[0];
+            const string &replacement = rule[1];
+
             int l = pattern.size();
 
             int this_cost = check(idx, l, replacement, pattern);
@@ -69,32 +79,36 @@ public:
         }
 
         if (source[idx] == target[idx]) {
+
             int solve_next = solve(idx + 1);
-            if (solve_next != -1) {
+
+            if (solve_next != -1)
                 curr_cost = min(curr_cost, solve_next);
-            }
         }
-        if (curr_cost == INT_MAX) {
+
+        if (curr_cost == INT_MAX)
             return dp[idx] = -1;
-        }
 
         return dp[idx] = curr_cost;
     }
 
-    int minCost(string source, string target, vector<vector<string>>& rules,
-                vector<int>& costs) {
-        
-        
-        if(source==target) return 0;
+    int minCost(string source, string target,
+                const vector<vector<string>> &rules,
+                const vector<int> &costs) {
+
+        if (source == target)
+            return 0;
+
         this->source = source;
         this->target = target;
         this->rules = rules;
         this->costs = costs;
+
         n = source.size();
-        for(int i=0;i<5001;i++){
-            dp[i]=-5;
-        }
-        int ans = solve(0);
-        return ans;
+
+        for (int i = 0; i < 5001; i++)
+            dp[i] = -5;
+
+        return solve(0);
     }
 };
