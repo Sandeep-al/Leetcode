8ba0fd671be462b1133ed84e29923f2af0558d01
SANDEEP <sandeep24504@iiitd.ac.in>
2026-07-30 15:13:08 +0530

    Time: 5 ms (1.39%) | Memory: 10.3 MB (7.95%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     int minimumPushes(string word) {
+        unordered_map<char, int> freq;
+
+        for (char c : word)
+            freq[c]++;
+        vector<pair<char, int>> v;
+
+        for (auto& it : freq)
+            v.push_back(it);
+        sort(v.begin(), v.end(),
+             [](const auto& a, const auto& b) { return a.second > b.second; });
         unordered_map<char, int> mpp;
         int count = 0;
         int curr = 1;
         int assigned = 0;
-        for (auto& it : word) {
+        for (auto& jt : v) {
+            auto it = jt.first;
             if (mpp.find(it) != mpp.end()) {
                 count += mpp[it];
             } else {
