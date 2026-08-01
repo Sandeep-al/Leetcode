c58275669bc89bd959c4673f78c942171703df3f
SANDEEP <sandeep24504@iiitd.ac.in>
2026-08-01 09:39:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (56.39%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     vector<int> nums;
+    int dp[21][21];
     int solve(int i, int j) {
         if (i > j) {
             return 0;
         }
         // case 1 take nums[i];
-
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
         int maxi = 0;
 
         int takeLeft = nums[i] + min(solve(i + 2, j), solve(i + 1, j - 1));
 
         int takeRight = nums[j] + min(solve(i, j - 2), solve(i + 1, j - 1));
 
-        return max(takeLeft, takeRight);
+        return dp[i][j]=max(takeLeft, takeRight);
     }
     bool predictTheWinner(vector<int>& nums) {
         this->nums = nums;
@@ -22,6 +25,7 @@ public:
         for (auto& it : nums) {
             target += it;
         }
+        memset(dp,-1,sizeof(dp));
         target = ceil((double)target / 2.0);
         int ans = solve(0, n - 1);
         cout << ans;
