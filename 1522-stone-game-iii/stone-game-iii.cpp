f2beaab4a4367f0d2e329c0e745f3be1d7eb03f8
SANDEEP <sandeep24504@iiitd.ac.in>
2026-08-03 13:59:02 +0530

    Time: 12 ms (86.94%) | Memory: 141.9 MB (37.98%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -1,37 +1,49 @@
 class Solution {
 public:
-    int n;
     vector<int> nums;
-    int dp[50005];
-    int solve(int i) {
+    // int dp[50005];
+    // int solve(int i) {
 
-        if (i == n)
-            return 0;
-        if (dp[i] != INT_MAX) {
-            return dp[i];
-        }
-        int maxi = INT_MIN;
-        int curr_score = 0;
-        for (int x = i; x < min(i + 3, n); x++) {
-            curr_score += nums[x];
-            maxi = max(maxi, curr_score - solve(x + 1));
-        }
+    //     if (i == n)
+    //         return 0;
+    //     if (dp[i] != INT_MAX) {
+    //         return dp[i];
+    //     }
+    //     int maxi = INT_MIN;
+    //     int curr_score = 0;
+    //     for (int x = i; x < min(i + 3, n); x++) {
+    //         curr_score += nums[x];
+    //         maxi = max(maxi, curr_score - solve(x + 1));
+    //     }
 
-        return dp[i] = maxi;
-    }
+    //     return dp[i] = maxi;
+    // }
     string stoneGameIII(vector<int>& stoneValue) {
-        n = stoneValue.size();
+        int n = stoneValue.size();
         nums = stoneValue;
-        
-        for (int i = 0; i < 50005; i++) {
-            dp[i] = INT_MAX;
+
+        vector<int> dp(n + 1);
+        dp[n] = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int maxi = INT_MIN;
+            int curr_score = 0;
+
+            for (int x = i; x < min(i + 3, n); x++) {
+                curr_score += nums[x];
+                maxi = max(maxi, curr_score - dp[x + 1]);
+            }
+
+            dp[i] = maxi;
         }
-        int ans = solve(0);
-        if (ans > 0) {
+
+        if(dp[0]>0){
             return "Alice";
-        } else if (ans < 0) {
+        }
+        else if(dp[0]<0){
             return "Bob";
         }
+
         return "Tie";
     }
 };
