8aa8eb888b20cef7cb4664e7a47da79319b05b1c
SANDEEP <sandeep24504@iiitd.ac.in>
2026-08-03 14:15:56 +0530

    Time: 3 ms (99.18%) | Memory: 136.2 MB (76.04%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -1,23 +1,7 @@
 class Solution {
 public:
     vector<int> nums;
-    // int dp[50005];
-    // int solve(int i) {
 
-    //     if (i == n)
-    //         return 0;
-    //     if (dp[i] != INT_MAX) {
-    //         return dp[i];
-    //     }
-    //     int maxi = INT_MIN;
-    //     int curr_score = 0;
-    //     for (int x = i; x < min(i + 3, n); x++) {
-    //         curr_score += nums[x];
-    //         maxi = max(maxi, curr_score - solve(x + 1));
-    //     }
-
-    //     return dp[i] = maxi;
-    // }
     string stoneGameIII(vector<int>& stoneValue) {
         int n = stoneValue.size();
         nums = stoneValue;
