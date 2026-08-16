9666d899e4d72a4ba328179d5ed356d590ccb062
SANDEEP <sandeep24504@iiitd.ac.in>
2026-08-16 10:49:20 +0530

    Time: 44 ms (54.86%) | Memory: 101.2 MB (54.09%) - LeetSync

--- 2549-next-greater-element-iv/next-greater-element-iv.cpp
+++ 2549-next-greater-element-iv/next-greater-element-iv.cpp
@@ -2,34 +2,28 @@ class Solution {
 public:
     vector<int> secondGreaterElement(vector<int>& nums) {
         int n = nums.size();
+        stack<int> st1;
+        stack<int> st2;
         vector<int> ans(n, -1);
-
-        stack<int> st1, st2;
-
         for (int i = 0; i < n; i++) {
+            int curr = nums[i];
 
-            // Current element is the SECOND greater
-            // for elements already waiting in st2
-            while (!st2.empty() && nums[st2.top()] < nums[i]) {
+            while (!st2.empty() && nums[st2.top()] < curr) {
                 ans[st2.top()] = nums[i];
                 st2.pop();
             }
-
             vector<int> temp;
-
-            // Current element is the FIRST greater
-            // for elements in st1
-            while (!st1.empty() && nums[st1.top()] < nums[i]) {
+            while (!st1.empty() && nums[st1.top()] < curr) {
                 temp.push_back(st1.top());
                 st1.pop();
             }
 
-            // Move them to second-greater waiting stack
-            for (int j = temp.size() - 1; j >= 0; j--) {
-                st2.push(temp[j]);
+            reverse(temp.begin(), temp.end());
+
+            for (int x = 0; x < temp.size(); x++) {
+                st2.push(temp[x]);
             }
 
-            // Current element now waits for its first greater
             st1.push(i);
         }
 
