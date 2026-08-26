0ff7c4a0112c9bb56b44617fd3b6d3842c91c697
SANDEEP <sandeep24504@iiitd.ac.in>
2026-08-26 16:20:20 +0530

    Time: 124 ms (6.92%) | Memory: 9.9 MB (46.54%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     int n;
-    vector<int>nums;
-    int score(int idx1,int idx2,int parity){
-        if(idx1>idx2){
+    vector<int> nums;
+
+    int score(int idx1, int idx2, int parity) {
+        if (idx1 > idx2)
             return 0;
-        }
-        int maxi=0;
-        if(parity==0){
-            maxi=max(maxi,nums[idx1]+score(idx1+1,idx2,1));
-            maxi=max(maxi,nums[idx2]+score(idx1,idx2-1,1));
-        }
-        else{
-            int left=score(idx1+1,idx2,0);
-            int right=score(idx1,idx2-1,0);
 
-            maxi=min(left,right);
+        if (parity == 0) {
+            int left = nums[idx1] + score(idx1 + 1, idx2, 1);
+            int right = nums[idx2] + score(idx1, idx2 - 1, 1);
+
+            return max(left, right);
         }
+        else {
+            int left = score(idx1 + 1, idx2, 0);
+            int right = score(idx1, idx2 - 1, 0);
 
-        return maxi;
-
+            return min(left, right);
+        }
     }
+
     bool predictTheWinner(vector<int>& nums) {
-        
-        n=nums.size();
-        this->nums=nums;
-        double total=0;
-        for(auto &it:nums){
-            total+=it;
-        }
-        total=total/2;
-        return score(0,n-1,0)>=total;
+        this->nums = nums;
+        n = nums.size();
+
+        int total = 0;
+
+        for (int x : nums)
+            total += x;
+
+        int player0Score = score(0, n - 1, 0);
+
+        return player0Score >= total - player0Score;
     }
 };
