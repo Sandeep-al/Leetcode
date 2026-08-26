e62e52e2c6d9185d8916b2bc281c7f64e7786136
SANDEEP <sandeep24504@iiitd.ac.in>
2026-08-26 19:44:41 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (20.37%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -4,27 +4,26 @@ public:
     int target;
     vector<vector<int>> final_ans;
     vector<int> nums;
-    vector<int> used;
+
     void solve(int idx, vector<int>& curr, int curr_sum) {
 
         if (curr_sum == target) {
             final_ans.push_back(curr);
             return;
         }
-        
+
         for (int i = idx; i < n; i++) {
 
-            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+            if (i > idx && nums[i] == nums[i - 1]) {
                 continue;
             }
             if (nums[i] + curr_sum <= target) {
                 curr_sum += nums[i];
                 curr.push_back(nums[i]);
-                used[i] = 1;
+
                 solve(i + 1, curr, curr_sum);
                 curr_sum -= nums[i];
                 curr.pop_back();
-                used[i] = 0;
             }
         }
     }
@@ -34,7 +33,7 @@ public:
         nums = candidates;
         vector<int> curr;
         sort(nums.begin(), nums.end());
-        used.assign(n, 0);
+        
         solve(0, curr, 0);
         return final_ans;
     }
