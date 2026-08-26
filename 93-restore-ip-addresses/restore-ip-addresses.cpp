aeab1511f15aaf57ede8592b191ed0e24a45e72a
SANDEEP <sandeep24504@iiitd.ac.in>
2026-08-26 23:17:14 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (79.06%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.cpp
+++ 93-restore-ip-addresses/restore-ip-addresses.cpp
@@ -1,65 +1,54 @@
 class Solution {
 public:
-    vector<string> temp;
-    vector<string> final_ans;
+    vector<string> ans;
+    string curr = "";
     int n;
+    string s;
 
-    bool isvalid(string& y, int start, int end) {
-        int len = end - start + 1;
-        
-        // 1. Length > 3 nahi honi chahiye (waise loop sambhal raha hai, but safety)
-        if (len > 3) return false;
+    void solve(int idx, int parts) {
 
-        // 2. Leading Zero Check: "0" allowed hai, par "01", "00" nahi
-        if (y[start] == '0' && len > 1) return false;
+        if (idx == n) {
+            if (parts == 4) {
 
-        // 3. Value > 255 Check
-        // Note: stoi ke liye string extract karni padegi
-        long long val = stoll(y.substr(start, len)); 
-        if (val > 255) return false;
-
-        return true;
-    }
-
-    void compute(int i, string& s) {
-        
-        if (i == n) {
-           
-            if (temp.size() == 4) {
-                string x = "";
-                for (int k = 0; k < temp.size(); k++) {
-                    x += temp[k];
-                    if (k < temp.size() - 1) x += ".";
-                }
-                final_ans.push_back(x);
+                ans.push_back(curr);
+                ans.back().pop_back();
             }
             return;
         }
-        
-       
-        if (temp.size() >= 4) return;
+        if (parts == 4) {
+            return;
+        }
+        // leading zeroes case;
+        if (s[idx] == '0') {
+            curr += "0.";
+            solve(idx + 1, parts + 1);
+            curr.pop_back();
+            curr.pop_back();
+        } else {
 
-       
-        for (int j = i; j < min(i + 3, n); j++) {
-            
-            
-            if (isvalid(s, i, j)) {
-                
-                
-                temp.push_back(s.substr(i, j - i + 1));
-                
-                compute(j + 1, s);
-                
-                temp.pop_back(); 
+            for (int i = idx; i < min(n, idx + 3); i++) {
+
+                string temp = s.substr(idx, i - idx + 1);
+
+                if (stoi(temp) <= 255) {
+                    curr += temp;
+                    curr.push_back('.');
+
+                    solve(i + 1, parts + 1);
+
+                    int len = i - idx + 2;
+                    while (len > 0) {
+                        curr.pop_back();
+                        len--;
+                    }
+                }
             }
         }
     }
-
     vector<string> restoreIpAddresses(string s) {
         n = s.size();
-       
-        if (n > 12) return {}; 
-        compute(0, s);
-        return final_ans;
+        this->s = s;
+        solve(0, 0);
+        return ans;
     }
 };
