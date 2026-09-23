31ff480c19cf6f08666d4495f4bc33dfbfd6ea77
SANDEEP <sandeep24504@iiitd.ac.in>
2026-09-23 19:23:28 +0530

    Time: 40 ms (22.94%) | Memory: 111.6 MB (22.16%) - LeetSync

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+
+        int n = nums.size();
+        vector<int> prefix(n, 0);
+        prefix[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+
+        vector<int> suffix(n, 0);
+        suffix[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + nums[i];
+        }
+
+        int ans = INT_MAX;
+
+        // we will pick elements from left and check how many we have to pick
+        // from the right
+
+        for (int i = -1; i < n; i++) {
+
+            // tell the sum till ith 
+            
+
+            int curr_sum =(i==-1)?0: prefix[i];
+
+            int new_x = x - curr_sum;
+            if (new_x < 0) {
+                break;
+            }
+
+            if (new_x == 0) {
+                ans = min(ans, i + 1);
+            }
+
+            // target is new_x
+
+            int l = i + 1;
+            int r = n - 1;
+            int idx = -1;
+            while (l <= r) {
+                int mid = (r - l) / 2 + l;
+
+                if (suffix[mid] == new_x) {
+                    idx = mid;
+                    break;
+                } else if (suffix[mid] > new_x) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+
+            if (idx != -1) {
+                int count = n - idx;
+                ans = min(ans, count + i + 1);
+            }
+        }
+
+        return (ans == INT_MAX) ? -1 : ans;
+    }
+};
