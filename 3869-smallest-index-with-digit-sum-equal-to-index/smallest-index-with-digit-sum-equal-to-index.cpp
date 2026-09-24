06ebd0941543bb15856fa57f365b3a36b9084114
SANDEEP <sandeep24504@iiitd.ac.in>
2026-09-24 08:59:49 +0530

    Time: 0 ms (100.00%) | Memory: 30.9 MB (81.66%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.cpp
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.cpp
@@ -4,11 +4,13 @@ public:
         int sum=0;
         while(x>0){
             sum+=(x%10);
-            x=x/10;
+            x/=10;
         }
+
         return sum;
     }
     int smallestIndex(vector<int>& nums) {
+        
         for(int i=0;i<nums.size();i++){
             if(digisum(nums[i])==i){
                 return i;
